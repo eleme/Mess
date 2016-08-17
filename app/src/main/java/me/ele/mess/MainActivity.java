3167e71f551856ec0a3ac6ec6419bbd3b032f107
peacepassion <daqi1990@gmail.com>
2016-08-17 17:34:05 +0800

    setup demo

--- app/src/main/java/me/ele/mess/MainActivity.java
+++ app/src/main/java/me/ele/mess/MainActivity.java
@@ -3,16 +3,22 @@ package me.ele.mess;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Toast;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+  @BindView(R.id.rv) RecyclerView rv;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
 
     CustomView customView = (CustomView) findViewById(R.id.custom_view);
     customView.setOnClickListener(new View.OnClickListener() {
@@ -21,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(MainActivity.this, "haha", Toast.LENGTH_SHORT).show();
       }
     });
+
+    rv.setAdapter(new MyAdapter());
   }
 
   @OnClick(R.id.btn) public void onClickBtn(View v) {
